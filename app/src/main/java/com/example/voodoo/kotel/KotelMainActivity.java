52bcb3ddf68b2f258adaf5f8c302152bee5700b5
tsbp <tsbp82@gmail.com>
2015-06-15 16:23:13 +0300

    1

--- app/src/main/java/com/example/voodoo/kotel/KotelMainActivity.java
+++ app/src/main/java/com/example/voodoo/kotel/KotelMainActivity.java
@@ -1,41 +1,49 @@
 package com.example.voodoo.kotel;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.Menu;
+import android.view.View;
 import android.view.Window;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 
-public class KotelMainActivity extends ActionBarActivity {
+public class KotelMainActivity extends Activity implements OnClickListener{
+
+    Button updBtn, setBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //activity_kotel_main.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_kotel_main);
+
+        Button updBtn = (Button) findViewById(R.id.updtbtn);
+        Button setBtn = (Button) findViewById(R.id.setbtn);
+
+        TextView inTemp = (TextView) findViewById(R.id.inTemp);
+        inTemp.setShadowLayer(5, 2, 2, Color.BLACK);
+
+        TextView outTemp = (TextView) findViewById(R.id.outTemp);
+        outTemp.setTextColor(Color.WHITE);
+        outTemp.setShadowLayer(5, 2, 2, Color.BLACK);
+
+        setBtn.setOnClickListener(this);
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_kotel_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.setbtn:
+                Intent intent = new Intent(this, settingsActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
 }
