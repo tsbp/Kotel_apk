7a54a853761ed33e5cc96cd32e9389b517a521ba
tsbp <tsbp82@gmail.com>
2015-06-30 16:56:42 +0300

    continue work on configs

--- app/src/main/java/com/example/voodoo/kotel/settingsActivity.java
+++ app/src/main/java/com/example/voodoo/kotel/settingsActivity.java
@@ -1,46 +1,48 @@
 package com.example.voodoo.kotel;
 
-import com.example.voodoo.kotel.util.SystemUiHider;
-import com.example.voodoo.plot;
-import com.example.voodoo.plot2;
-
-import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.preference.PreferenceManager;
-import android.view.MotionEvent;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.view.View.OnClickListener;
 import android.widget.GridView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.ProgressBar;
-import android.widget.TableLayout;
-import android.widget.TableRow;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketTimeoutException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class settingsActivity extends Activity {
 
     Button bSave, bLoad, bAdd, bDel;
     String configReference = "lanConfig";
-    String[] time = {"12.30", "18.30", "22.30", "00.30"};
-    String[] temp = {"22", "24", "26.7", "13.9"};
+    String[] time;
+    String[] temp;
+    ProgressBar pbConfig;
 
     GridView gvMain;
+    ListView lvMain;
     ArrayAdapter<String> adapter;
+    List<String> aStrings = new ArrayList<String>();
+    List<String> bStrings = new ArrayList<String>();
+
+    int selectedRow;
 
 
     @Override
@@ -49,21 +51,11 @@ public class settingsActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        adapter = new ArrayAdapter<String>(this, R.layout.item, R.id.tvText, time);
-        gvMain = (GridView) findViewById(R.id.gvTime);
-        gvMain.setAdapter(adapter);
-
-        adapter = new ArrayAdapter<String>(this, R.layout.item, R.id.tvText, temp);
-        gvMain = (GridView) findViewById(R.id.gvTemprature);
-        gvMain.setAdapter(adapter);
-
+        final TextView response = (TextView) findViewById(R.id.confResponse);
 
         Button bSave = (Button) findViewById(R.id.btnSave);
         Button bLoad = (Button) findViewById(R.id.btnLoad);
-        Button bAdd = (Button) findViewById(R.id.btnAdd);
-        Button bDel = (Button) findViewById(R.id.btnDel);
 
-        final TextView response = (TextView) findViewById(R.id.confResponse);
 
         //================================================
         bSave.setOnClickListener(new OnClickListener() {
@@ -74,27 +66,90 @@ public class settingsActivity extends Activity {
         //================================================
         bLoad.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
-                loadConfig();
-                //pbWait.setVisibility(View.VISIBLE);
 
+                ProgressBar pb = (ProgressBar)findViewById(R.id.pbConfig);
+                pb.setVisibility(View.VISIBLE);
+                loadConfig();
                 response.setText("Sending...");
                 SendTask tsk = new SendTask();
                 tsk.execute();
 
             }
         });
-        //================================================
-        bAdd.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
+//        //================================================
+//        bAdd.setOnClickListener(new OnClickListener() {
+//            public void onClick(View v) {
+//
+//            }
+//        });
+//        //================================================
+//        bDel.setOnClickListener(new OnClickListener() {
+//            public void onClick(View v) {
+//
+//            }
+//        });
+    }
+    //==============================================================================================
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (data == null) {
+            return;
+        }
+        String aTime = data.getStringExtra("rTime");
+        String aTemp = data.getStringExtra("rTemp");
+        time[selectedRow] = aTime;
+        temp[selectedRow] = aTemp;
+        updateConfigTable();
+    }
+    //==============================================================================================
+    // имена атрибутов для Map
+    final String ATTRIBUTE_NAME_TIME = "time";
+    final String ATTRIBUTE_NAME_TEMP = "temper";
+    //final String ATTRIBUTE_NAME_IMAGE = "image";
+    //==============================================================================================
+    void updateConfigTable()
+    {
+        int img = R.drawable.timeicon;
+
+        // упаковываем данные в понятную для адаптера структуру
+        ArrayList<Map<String, Object>> data = new ArrayList<Map<String, Object>>(
+                time.length);
+        Map<String, Object> m;
+        for (int i = 0; i < time.length; i++) {
+            m = new HashMap<String, Object>();
+            m.put(ATTRIBUTE_NAME_TIME, time[i]);
+            m.put(ATTRIBUTE_NAME_TEMP, temp[i]);
+            //m.put(ATTRIBUTE_NAME_IMAGE, img);
+            data.add(m);
+        }
+
+        // массив имен атрибутов, из которых будут читаться данные
+        String[] from = { ATTRIBUTE_NAME_TIME, ATTRIBUTE_NAME_TEMP};
+        // массив ID View-компонентов, в которые будут вставлять данные
+        int[] to = { R.id.tvTime, R.id.tvTemp };
+
+        // создаем адаптер
+        SimpleAdapter sAdapter = new SimpleAdapter(this, data, R.layout.item,
+                from, to);
+
+        // определяем список и присваиваем ему адаптер
+        lvMain = (ListView) findViewById(R.id.lvMain);
+        lvMain.setAdapter(sAdapter);
+        //==========================================================
+        lvMain.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                selectedRow = position;
+                Intent intent = new Intent(settingsActivity.this, PeroidConfig.class);
+                intent.putExtra("pTime", time[position]);
+                intent.putExtra("pTemp", temp[position]);
+                //startActivity(intent);
+                startActivityForResult(intent, 1);
 
             }
         });
-        //================================================
-        bDel.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
 
-            }
-        });
+
     }
     //==============================================================================================
     SharedPreferences sPref;
@@ -107,15 +162,12 @@ public class settingsActivity extends Activity {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         String  cString = sharedPreferences.getString(configReference, "") ;
 
-//        sPref = getPreferences(MODE_PRIVATE);
-//        String cString = sPref.getString(configReference, "");
         String _ip, _port;
 
-        int i = cString.indexOf("port");
-        if(i != -1)
+        if( cString.contains("port"))
         {
-            _ip   = cString.substring(0,i);
-            _port = cString.substring(i+4,cString.length());
+            _ip   = cString.substring(0,cString.indexOf("port"));
+            _port = cString.substring(cString.indexOf("port")+4,cString.length());
         }
         else return;
 
@@ -196,8 +248,6 @@ public class settingsActivity extends Activity {
             return null;
         }
 
-
-
         @Override
         protected void onPostExecute(Void result) {
             super.onPostExecute(result);
@@ -207,6 +257,57 @@ public class settingsActivity extends Activity {
             TextView response = (TextView) findViewById(R.id.confResponse);
             response.setText(st);
 
+            int dataIndex = ret.indexOf("data:") + 5;
+            String resp = ret.substring(dataIndex, ret.length());
+            String s;
+
+            if((resp.indexOf("\n\r") == 10) && (resp.indexOf("C") == 0) )
+            {
+                resp = resp.substring(0, 10);
+                s = resp.substring(1, 2);
+                int msgNumb = Integer.parseInt(s);
+                s = resp.substring(2, 3);
+                int partsCount = Integer.parseInt(s);
+
+                if(msgNumb == partsCount)
+                {
+                    aStrings.add(resp.substring(3));
+                    bStrings.add(resp.substring(3));
+                    time = new String[aStrings.size()];
+                    temp = new String[bStrings.size()];
+                    time = aStrings.toArray(time);
+                    temp = bStrings.toArray(temp);
+
+                    for(int j = 0; j < time.length; j++)
+                        time[j] = time[j].substring(0,2) + ":" + time[j].substring(2,4);
+                    for(int j = 0; j < temp.length; j++)
+                    {
+                        String a1 = temp[j].substring(4, 6);
+                        String a2 = temp[j].substring(6);
+                        temp[j] = a1 + "," + a2;
+                    }
+
+                    updateConfigTable();
+                    ProgressBar pb = (ProgressBar)findViewById(R.id.pbConfig);
+                    pb.setVisibility(View.INVISIBLE);
+                    aStrings.clear();
+                    bStrings.clear();
+                    REQUEST_ACTION = "CONF1";
+                }
+                else
+                {
+                    aStrings.add(resp.substring(3));
+                    bStrings.add(resp.substring(3));
+                    REQUEST_ACTION = "CONF" + String.valueOf(msgNumb+1);
+                    SendTask tsk = new SendTask();
+                    tsk.execute();
+                }
+            }
+            else
+            {
+                SendTask tsk = new SendTask();
+                tsk.execute();
+            }
             ret = "no answer";
         }
 
